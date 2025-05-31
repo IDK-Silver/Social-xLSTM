663a8c204effbdb8d7dfc26dcfa11a30bffde4b0
yu-feng <a288235403@gmail.com>
2025-05-31 09:35:57 +0000

    Refactor plot_vd_coordinates function to use VDInfo for loading JSON data and remove command-line argument parsing

--- src/social_xlstm/utils/graph.py
+++ src/social_xlstm/utils/graph.py
@@ -1,20 +1,11 @@
 import argparse
 import json
 import matplotlib.pyplot as plt
-from convert_coords import mercator_projection
+from .convert_coords import mercator_projection
 from social_xlstm.dataset.json_utils import VDLiveList, VDInfo
 #python graph.py --VDListJson /home/GP/repo/Social-xLSTM/blob/lab/2025-03-18_00-49-00/VDList.json
 
 
-def parse_arguments():
-    """
-    Parse command-line arguments using argparse.
-    """
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--VDListJson", required=True, help="Json檔絕對位置")
-
-    return parser.parse_args()
 
 
 def plot_vd_coordinates(json_path, lat_origin=23.9150, lon_origin=120.6846, radius=6378137):
@@ -25,20 +16,22 @@ def plot_vd_coordinates(json_path, lat_origin=23.9150, lon_origin=120.6846, radi
     if json_path is None:
         raise ValueError("json_path cannot be None")
     
-    try:
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-    except Exception as e:
-        print(f"無法讀取 JSON 檔案：{e}")
-        return
+    # try:
+    #     with open(json_path, "r", encoding="utf-8") as f:
+    #         data = json.load(f)
+    # except Exception as e:
+    #     print(f"無法讀取 JSON 檔案：{e}")
+    #     return
+    
+    vd_info=VDInfo.load_from_json(json_path)
 
     x_coords = []
     y_coords = []
 
-    for vd in data.get("VDList", []):
+    for vd in vd_info.VDList:
         try:
-            lat = vd["PositionLat"]
-            lon = vd["PositionLon"]
+            lat = vd.PositionLat
+            lon = vd.PositionLon
             x, y = mercator_projection(lat, lon, lat_origin, lon_origin, radius)
             x_coords.append(x)
             y_coords.append(y)
@@ -60,9 +53,3 @@ def plot_vd_coordinates(json_path, lat_origin=23.9150, lon_origin=120.6846, radi
     plt.tight_layout()
     plt.savefig("/home/GP/repo/Social-xLSTM/src/social_xlstm/utils/vdid_map.png", dpi=1000)
 
-def main():
-    args = parse_arguments()
-    plot_vd_coordinates(args.VDListJson)
-
-if __name__ == "__main__":
-    main()
